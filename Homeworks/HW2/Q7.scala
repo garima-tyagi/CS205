7af9aac9c71cf74b06d3574a00495f72cd692a77
garima-tyagi <garima.tyagi_ug18@ashoka.edu.in>
2016-09-14 00:34:33 +0530

    Update Q7.scala

--- Homeworks/HW2/Q7.scala
+++ Homeworks/HW2/Q7.scala
@@ -1,15 +1,13 @@
 object Q7 {
 def main (args: Array[String]) {
-println ("Result is" + fastPower(1,2))
+println ("Result is" + fastPower(2,3))
 }
 
 def fastPower(X: Int, N: Int) : Int = { 
-	var res= fastPower(X, N/2)
-	var oddRes= fastPower(X, (N-1)/2)
 	N match{
 		case 0 => 1
-		case n if (n%2==0) => res*res
-		case _ => X*oddRes*oddRes
+		case n if (n%2==0) => fastPower(X,N/2)*fastPower(X,N/2)
+		case _ => X*fastPower(X, N-1)
 
 }
 }	
