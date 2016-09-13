2319b7421461d1bc12deb479af0c2d9c5e6832fb
garima-tyagi <garima.tyagi_ug18@ashoka.edu.in>
2016-09-13 23:58:43 +0530

    Update Q6.scala

--- Homeworks/HW2/Q6.scala
+++ Homeworks/HW2/Q6.scala
@@ -1,15 +1,18 @@
 object Q6 {
 def main(args: Array[String]) {
-println ("Sorted List is: " + quickSort(List(2,3,1,7,4)))
+// Calls the function with an unsorted list
+	println ("Sorted List is after adding the element is: " + quickSort(List(2,3,1,7,4)))
 }
 // Function Definition
 	
-def quickSort(List1: List[Int]): List[Int]={
-	List1 match {
-		case hd::rest => (quickSort(pivotSort(List1.init,List1.last)))
-		case _ => List1
-	}
+def quickSort(List1: List[Int]): List[Int]= {
+List1 match{	
+case hd::rest => pivotSort(quickSort(rest), hd)
+case _ => List1
 }
+}
+
+//Uses pivotSort to sort the list
 def pivotSort(List1: List[Int], I: Int): List[Int]= {
 	List1 match {
 		case Nil => (I::List1)
@@ -18,11 +21,10 @@ def pivotSort(List1: List[Int], I: Int): List[Int]= {
 			else (hd::I::Nil)
 		}
 		case hd::rest => {
-			if (hd> I) (pivotSort(rest, I):::List(hd))
+			if (hd> I) (I::hd::rest)
 			else (hd::pivotSort(rest, I))
-		
 			}
 			}
-	
 }
 }
+
