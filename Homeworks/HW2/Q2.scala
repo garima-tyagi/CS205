b91f82fae2bd375eca334a037b7f0c89b66458d2
garima-tyagi <garima.tyagi_ug18@ashoka.edu.in>
2016-09-13 22:34:45 +0530

    Update Q2.scala

--- Homeworks/HW2/Q2.scala
+++ Homeworks/HW2/Q2.scala
@@ -1,17 +1,17 @@
 object Q2 {
 def main(args:Array[String]) {
 	
-	// Calls the function fact with the entered input, and displays the function's result 
+	// Calls the function split with a list, and displays the function's result 
 	println( "Lists are: " + Split(List(1,4,5,2,7)))
 }
-// This is the defination of the function to calculate factorial	
+// This is the defination of the function to split the list into two halves	
 def Split(List1: List[Int]): (List[Int], List[Int] )={
 	 
 	List1 match{
 		case Nil => (Nil, Nil)
 		case hd::Nil => (List1,Nil)
 		case hd::rest => {
-			var(first, sec)= Split((List1.init).drop(1));
+			var(first, sec)= Split((List1.init).drop(1)); //Calling split recursively on the list wothout the first and the last element
 			if (List1==List1.head::List1.last::Nil) (List(List1.head), List(List1.last))
 			else (hd::first, rest.last::sec)
 			
