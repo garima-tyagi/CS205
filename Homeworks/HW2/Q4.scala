9dbc5c1dce41960d1b81d4f209b89fdccc157c7d
garima-tyagi <garima.tyagi_ug18@ashoka.edu.in>
2016-09-13 22:40:00 +0530

    Update Q4.scala

--- Homeworks/HW2/Q4.scala
+++ Homeworks/HW2/Q4.scala
@@ -1,9 +1,10 @@
 object Q4 {
 def main(args: Array[String]) {
-// Calls the function with a list and an integer to be inserted
+// Calls the function InsertionSort with an unsorted list
 println ("Sorted List is: " + insertionSort(List(2,3,1,5,4)))
 }
 // Function Definition
+//After call from insertionSort
 def sortInsert(List1: List[Int], I: Int): List[Int]= {
 	List1 match {
 		case Nil => (I::List1)
@@ -17,9 +18,10 @@ def sortInsert(List1: List[Int], I: Int): List[Int]= {
 			}
 			}
 }
+//After the call from main
 def insertionSort(List1: List[Int]): List[Int]= {
 List1 match{	
-case hd::rest => sortInsert(insertionSort(rest), hd)
+case hd::rest => sortInsert(insertionSort(rest), hd)//Sort the list recursively
 case _ => List1
 }
 }
