a4339abe043f8af267d70ce711fd7ca0f194938b
garima-tyagi <garima.tyagi_ug18@ashoka.edu.in>
2016-09-13 23:41:16 +0530

    Update Q6.scala

--- Homeworks/HW2/Q6.scala
+++ Homeworks/HW2/Q6.scala
@@ -1,13 +1,12 @@
 object Q6 {
 def main(args: Array[String]) {
-// Calls the function with a list and an integer to be inserted
 println ("Sorted List is: " + quickSort(List(2,3,1,7,4)))
 }
 // Function Definition
 	
 def quickSort(List1: List[Int]): List[Int]={
 	List1 match {
-		case hd::rest => pivotSort(quickSort(rest),hd)
+		case hd::rest => (quickSort(pivotSort(List1.init,List1.last)))
 		case _ => List1
 	}
 }
@@ -21,7 +20,9 @@ def pivotSort(List1: List[Int], I: Int): List[Int]= {
 		case hd::rest => {
 			if (hd> I) (pivotSort(rest, I):::List(hd))
 			else (hd::pivotSort(rest, I))
+		
 			}
 			}
+	
 }
 }
